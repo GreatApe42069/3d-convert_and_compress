532b8a0e545607601e084f63e7abf10c9350cce1
GreatApe42069 <153969184+GreatApe42069@users.noreply.github.com>
2024-03-01 01:12:17 -0500

    Add files via upload

---
+++ 3d-convert_and_compress.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+def convert_to_gltf(input_file, output_file):
+    subprocess.run(["gltfpack", "-i", input_file, "-o", output_file])
+
+def main():
+    input_folder = "C:\Doginals-main\Dogemap-Dogs"  # Set your input folder here
+    output_folder = "C:\Doginals-main\Dogemap-Dogs\Converted"  # Set your output folder here
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for file in os.listdir(input_folder):
+        if file.endswith((".obj", ".fbx", ".usdz")):
+            input_path = os.path.join(input_folder, file)
+            output_path = os.path.join(output_folder, os.path.splitext(file)[0] + ".gltf")
+
+            convert_to_gltf(input_path, output_path)
+
+if __name__ == "__main__":
+    main()
